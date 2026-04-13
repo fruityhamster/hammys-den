e9b5cf38bf9fecb377bc30a3d64cbb92a21411b4
fruityhamster <sara.madalena.martinho@gmail.com>
2026-04-13 17:54:04 +0100

    dashboard started
    dashboard and some base classes

--- src/main/main.js
+++ src/main/main.js
@@ -3,12 +3,18 @@ const path = require('path');
 
 function createWindow() {
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 450,
+    height: 550,
+    useContentSize: true,
+    resizable: false,       // Impede o utilizador de esticar a janela
+    maximizable: false,     // Desativa o botão de maximizar
+    fullscreenable: false,  // Impede que entre em ecrã total
+    frame: false,           // Mantém a barra superior (ou false se quiseres tirar)
+    transparent: true,      // Permite que os cantos arredondados funcionem
     webPreferences: {
       nodeIntegration: true,
-      contextIsolation: false
-    }
+      contextIsolation: false,
+    },
   });
 
   win.loadURL('http://localhost:5173/'); // Aponta para o Vite dev server
--- src/renderer/pages/App.jsx
+++ src/renderer/pages/App.jsx
@@ -2,19 +2,16 @@
 import TodoList from "../components/TodoList";
 import Timer from "../components/Timer";
 import MyCalendar from "../components/Calendar";
+import Dashboard from "../components/Dashboard";
 /*  import Login from '../components/login';
-    import Dashboard from '../components/Dashboard';
     import History from '../components/history';
 */
 
 function App() {
   return (
-    <div className={"bg-hammy-beige-dark p-4 text-hammy-brown-dark"}>
-        <div style={{ padding: "20px" }}>
-          <h1>Aplicação de Produtividade</h1>
-          <TodoList />
-          <Timer />
-          <MyCalendar />
+    <div>
+        <div>
+          <Dashboard />
         </div>
     </div>
     
