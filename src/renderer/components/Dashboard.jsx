6262398e64677f7ff37998e53b27be92520e9963
fruityhamster <sara.madalena.martinho@gmail.com>
2026-04-14 14:50:10 +0100

    dashboard update
    - dashboard: tittle (draggable) ; min&close buttons actions and animations; 4 main buttons with image, text and animation.
    - missing: finish calendar button.

--- src/renderer/components/Dashboard.jsx
+++ src/renderer/components/Dashboard.jsx
@@ -1,24 +1,40 @@
 import React from 'react';
 
+// images imports
+import to_do_list from '../assets/dashboard-to-do-list.png'; 
+import calendar from '../assets/dashboard-calendar.png';
+import timer from '../assets/dashboard-timer.png';
+import history from '../assets/dashboard-history.png';
+
 const Dashboard = ({ onNavigate }) => {
+    // buttons configurations - avoids repeting code
+    const modules = [
+        { id: 'todo', label:'to-do list', img: to_do_list },
+        { id: 'calendar', label:'calendar', img: calendar },
+        { id: 'timer', label:'timer', img: timer },
+        { id: 'history', label:'history', img: history },
+    ];
 
-// buttons configurations - avoids repeting code
-const modules = [
-    { id: 'to-do', label: 'to-do', icon: '🌱' },
-    { id: 'calendar', label: 'calendar', icon: '📅' },
-    { id: 'timer', label: 'timer', icon: '⏳' },
-    { id: 'history', label: 'history', icon: '🌻' },
-];
-
-// minimize app
-const minimizeApp = () => {
-    if (window.require) {
-        const { ipcRenderer } = window.require('electron');
-        ipcRenderer.send('minimize-app');
-    } else {
-        console.warn("Electron IPC não encontrado");
+    // minimize app
+    const minimizeApp = () => {
+        if (window.require) {
+            const { ipcRenderer } = window.require('electron');
+            // wait for animation
+            setTimeout(() => {
+                ipcRenderer.send('minimize-app');
+            }, 150);
+        } else {
+            console.warn("Electron IPC não encontrado");
+        };
     };
-};
+
+    // close app
+    const closeApp = () => {
+        // wait for animation
+        setTimeout(() => {
+            window.close();
+        }, 150);
+    };
 
     return (
         <div className="app-container">
@@ -28,14 +44,21 @@ const minimizeApp = () => {
                 {/* buttons min&close (not draggable) */}
                 <div className="flex gap-1" style={{ WebkitAppRegion: 'no-drag' }}>
                     <button className="min-close-buttons" onClick={minimizeApp}>_</button>
-                    <button className="min-close-buttons" onClick={() => window.close()}>x</button>
+                    <button className="min-close-buttons" onClick={closeApp}>x</button>
                 </div>
             </div>
 
             <div className="base-background bg-[var(--color-hammy-brown)]"></div>
 
-            <div className="dashboard"></div>
-
+            <div className="dashboard" style={{ WebkitAppRegion: 'no-drag' }}>
+                {modules.map((mod) => (
+                    <button key={mod.id} className="grid-button" onClick={() => onNavigate(mod.id)}>
+                        <img src={mod.img} alt="" draggable="false"/>
+                        <span>{mod.label}</span> 
+                    </button>
+                    
+                ))}
+            </div>
         </div>
     );
 };
