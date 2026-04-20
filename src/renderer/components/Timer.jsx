9c44804826a1db45287e2c51331abc52c834e6c6
fruityhamster <sara.madalena.martinho@gmail.com>
2026-04-20 14:38:05 +0100

    default timer page

--- src/renderer/components/Timer.jsx
+++ src/renderer/components/Timer.jsx
@@ -1,6 +1,7 @@
-import { useState, useEffect } from "react";
+import { useState } from "react"; /* useEffect */
 
-function Timer() {
+const Timer = ({ onBack }) => {
+  /* 
   const [seconds, setSeconds] = useState(1500); // 25 min
   const [running, setRunning] = useState(false);
 
@@ -12,16 +13,52 @@ function Timer() {
     return () => clearInterval(interval);
   }, [running]);
 
-  const formatTime = (sec) => `${Math.floor(sec/60)}:${sec % 60 < 10 ? "0" : ""}${sec % 60}`;
+ const formatTime = (sec) => `${Math.floor(sec/60)}:${sec % 60 < 10 ? "0" : ""}${sec % 60}`; */
+
+  // minimize app
+  const minimizeApp = () => {
+      if (window.require) {
+          const { ipcRenderer } = window.require('electron');
+          // wait for animation
+          setTimeout(() => {
+              ipcRenderer.send('minimize-app');
+          }, 150);
+      } else {
+          console.warn("Electron IPC não encontrado");
+      };
+  };
+
+  // close app
+  const closeApp = () => {
+      // wait for animation
+      setTimeout(() => {
+          window.close();
+      }, 150);
+  };
 
   return (
-    <div>
-      <h2>Timer</h2>
-      <h1>{formatTime(seconds)}</h1>
-      <button onClick={() => setRunning(!running)}>{running ? "Pause" : "Start"}</button>
-      <button onClick={() => setSeconds(1500)}>Reset</button>
+    <div className="app-container">
+      {/* superior bar (draggable) */}
+      <div className="flex justify-between items-center mb-6" style={{ WebkitAppRegion: 'drag' }}>
+        <div className="main-title">hammy's den &lt;3</div>
+        {/* buttons min&close (not draggable) */}
+        <div className="flex gap-1" style={{ WebkitAppRegion: 'no-drag' }}>
+            <button className="min-close-buttons" onClick={minimizeApp}>_</button>
+            <button className="min-close-buttons" onClick={closeApp}>x</button>
+        </div>
+      </div>
+      <div className="base-background"></div>
+
+      <h2 className="page-title">timer</h2>
+
+
+
+      {/* "home" button */}
+      <div className="flex justify-center">
+        <button onClick={onBack} className="button-center1">home</button>
+      </div>
     </div>
   );
-}
+};
 
 export default Timer;
