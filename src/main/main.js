b9928d9b6984e328435ce959c2f74b081187ee2e
fruityhamster <sara.madalena.martinho@gmail.com>
2026-05-11 16:25:10 +0100

    connection with prisma
    to-do list, calendar, timer, history

--- src/main/main.js
+++ src/main/main.js
@@ -1,5 +1,6 @@
-const { app, BrowserWindow, ipcMain } = require('electron');
-const path = require('path');
+import { app, BrowserWindow, ipcMain } from 'electron';
+import { PrismaClient } from '@prisma/client';
+import path from 'path';
 
 // defines App's ID so Windows recognises the icon in Taskbar
 app.setAppUserModelId("com.hammysden.app");
@@ -38,4 +39,106 @@ app.on('window-all-closed', () => {
 ipcMain.on('minimize-app', () => {
   const win = BrowserWindow.getFocusedWindow();
   if (win) win.minimize();
+});
+
+const prisma = new PrismaClient();
+
+// TO-DO LIST
+// add tasks
+ipcMain.handle('add-task', async (event, taskData) => {
+  return await prisma.task.create({
+    data: taskData
+  });
+});
+
+// load tasks
+ipcMain.handle('get-tasks', async (event, userId) => {
+  return await prisma.task.findMany({
+    where: { userId },
+    orderBy: { position: 'asc' }
+  });
+});
+
+// update task (check)
+ipcMain.handle('update-task', async (event, { id, data }) => {
+  return await prisma.task.update({
+    where: { id: id },
+    data: data
+  });
+});
+
+// delete task
+ipcMain.handle('delete-task', async (event, id) => {
+  return await prisma.task.delete({
+    where: { id: id }
+  });
+});
+
+// CALENDAR
+// load calendar events
+ipcMain.handle('get-events', async (event, userId) => {
+  return await prisma.event.findMany({
+    where: { userId: userId },
+    orderBy: { startDate: 'asc' }
+  });
+});
+
+// new event
+ipcMain.handle('add-event', async (event, eventData) => {
+  return await prisma.event.create({
+    data: eventData
+  });
+});
+
+// delete event
+ipcMain.handle('delete-event', async (event, id) => {
+  return await prisma.event.delete({
+    where: { id: id }
+  });
+});
+
+// update task (check)
+ipcMain.handle('update-event', async (event, { id, data }) => {
+  return await prisma.event.update({
+    where: { id: id },
+    data: data
+  });
+});
+
+// TIMER
+// save/add session
+ipcMain.handle('add-session', async (event, sessionData) => {
+  return await prisma.TimerSession.create({
+    data: {
+      duration: parseInt(sessionData.duration) || 0, // not null
+      recipe: sessionData.recipe,
+      notes: sessionData.notes,
+      // telling Prisma: "connect this session to this user ID"
+      user: {
+        connect: { id: sessionData.userId }
+      }
+    }
+  });
+});
+
+// update existing session (for History editing)
+ipcMain.handle('update-session', async (event, { id, data }) => {
+  return await prisma.TimerSession.update({
+    where: { id },
+    data: data
+  });
+});
+
+// HISTORY
+// get sessions history
+ipcMain.handle('get-sessions', async (event, userId) => {
+  try {
+    return await prisma.timerSession.findMany({
+      where: { userId: userId },
+      orderBy: { createdAt: 'desc' } // shows the most recents first
+    });
+  } catch (error) {
+    console.error("Erro Prisma (get-sessions):", error);
+    throw error;
+  }
 });
