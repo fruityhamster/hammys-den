a86232e7c1b052b951928e2480d3a324ad9f5b85
fruityhamster <sara.madalena.martinho@gmail.com>
2026-05-12 17:51:52 +0100

    + userId
    temporary user id removed

--- src/renderer/pages/App.jsx
+++ src/renderer/pages/App.jsx
@@ -7,36 +7,41 @@ import TodoList from "../components/TodoList";
 import CalendarPage from "../components/Calendar";
 import Timer from "../components/Timer";
 import History from '../components/History';
-/* import Login from '../components/login'; */
+import Login from '../components/Login';
 
 function App() {
-  // state to know what page we are on (log in ainda não está feito, começa na Dashboard)
+  const [user, setUser] = useState(null);
+  // state to know what page we are on
   const [currentPage, setCurrentPage] = useState('dashboard');
+  const [editingSession, setEditingSession] = useState(null);
 
   // function to change pages
   const navigateTo = (page) => setCurrentPage(page);
-
-  const [editingSession, setEditingSession] = useState(null);
 
   const openHistorySession = (session) => {
     setEditingSession(session); // saves the data from the clicked session
     setCurrentPage('timer');    // sends the user to the timer page
   };
 
+  // if there is no user, shows login page
+  if (!user) {
+    return <Login onLoginSuccess={(userData) => setUser(userData)} />;
+  }
+
   return (
     <div>
-      {/* app chooses what to render based on the state */}
-      {currentPage === 'dashboard' && <Dashboard onNavigate={navigateTo} />}
+      {/* if there is an user, send the ID of the user to everything */}
+      {currentPage === 'dashboard' && (<Dashboard onNavigate={navigateTo} user={user} />)}
 
       {/* if state changes to 'todo' shows 'To-do List' page */}
-      {currentPage === 'todo' && <TodoList onBack={() => setCurrentPage('dashboard')} />}
+      {currentPage === 'todo' && <TodoList onBack={() => setCurrentPage('dashboard')} userId={user.id} />}
 
       {/* if state changes to 'calendar' shows 'Calendar' page */}
-      {currentPage === 'calendar' && <CalendarPage onBack={() => setCurrentPage('dashboard')} />}
+      {currentPage === 'calendar' && <CalendarPage onBack={() => setCurrentPage('dashboard')} userId={user.id} />}
       
-      {currentPage === 'timer' && (<Timer onBack={() => { setCurrentPage('dashboard'); setEditingSession(null); }} editData={editingSession} />)}
+      {currentPage === 'timer' && (<Timer onBack={() => { setCurrentPage('dashboard'); setEditingSession(null);}} editData={editingSession} userId={user.id} />)}
 
-      {currentPage === 'history' && (<History onBack={() => setCurrentPage('dashboard')} onEditSession={openHistorySession} />)}
+      {currentPage === 'history' && (<History onBack={() => setCurrentPage('dashboard')} onEditSession={openHistorySession} userId={user.id} />)}
     </div>
   );
 }
