c01acde2b9e081ad2364bff1dde390312431dc39
fruityhamster <sara.madalena.martinho@gmail.com>
2026-05-19 22:07:40 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// Esta é a configuração que diz ao Vite para ativar o Tailwind
 export default defineConfig({
+  base: './', // so Electron finds assets
   plugins: [
     react(),
     tailwindcss(),
