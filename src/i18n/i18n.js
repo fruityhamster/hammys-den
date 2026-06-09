f83a069bdde2820f7cc7ab77621fa1d146f32347
fruityhamster <sara.madalena.martinho@gmail.com>
2026-06-09 09:12:13 +0100

    added "i18n"

---
+++ src/i18n/i18n.js
@@ -0,0 +1,23 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+
+import translationEN from './locales/en.json';
+import translationPT from './locales/pt.json';
+
+const resources = {
+  en: { translation: translationEN },
+  pt: { translation: translationPT }
+};
+
+i18n
+  .use(initReactI18next)
+  .init({
+    resources,
+    lng: 'en', // default language
+    fallbackLng: 'en', // if a key is missing in a language shows in english
+    interpolation: {
+      escapeValue: false 
+    }
+  });
+
+export default i18n;
